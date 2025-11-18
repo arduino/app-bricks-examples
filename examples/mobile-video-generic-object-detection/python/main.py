48d91bb1cc7005ee34b58176a50df7bd26e7a7e9
Roberto Gazia <r.gazia@arduino.cc>
2025-11-18 12:16:36 +0100

    use new state machine from Camera instance

--- examples/mobile-video-generic-object-detection/python/main.py
+++ examples/mobile-video-generic-object-detection/python/main.py
@@ -14,17 +14,19 @@ from datetime import datetime, UTC
 
 def generate_secret() -> str:
   characters = string.ascii_letters + string.digits
-  password = ''.join(secrets.choice(characters) for i in range(5))
-  return password
+  return ''.join(secrets.choice(characters) for i in range(5))
 
-password = generate_secret()
-
-camera = Camera("ws://0.0.0.0:8080")
-detection_stream = VideoObjectDetection(camera, confidence=0.5, debounce_sec=0.0)
+secret = generate_secret()
 
 ui = WebUI(use_ssl=True)
-ui.on_message("override_th", lambda sid, threshold: detection_stream.override_threshold(threshold))
-ui.on_connect(lambda sid: ui.send_message("secret", message=password))
+camera = Camera("ws://0.0.0.0:8080")
+
+ui.on_connect(lambda sid: ui.send_message("welcome", {"secret": secret, "status": camera.status}))
+camera.on_status_changed(lambda evt_type, data: ui.send_message(evt_type, data))
+
+detection = VideoObjectDetection(camera, confidence=0.5, debounce_sec=0.0)
+
+ui.on_message("override_th", lambda sid, threshold: detection.override_threshold(threshold))
 
 # Register a callback for when all objects are detected
 def send_detections_to_ui(detections: dict):
@@ -34,23 +36,8 @@ def send_detections_to_ui(detections: dict):
       "confidence": value.get("confidence"),
       "timestamp": datetime.now(UTC).isoformat()
     }
-    ui.send_message("detection", message=entry)
+    ui.send_message("detection", entry)
 
-detection_stream.on_detect_all(send_detections_to_ui)
+detection.on_detect_all(send_detections_to_ui)
 
-def user_loop():
-  # WIP: Simulate webcam connection and streaming events
-  time.sleep(10)
-  ui.send_message("webcam_connected", message="")
-  time.sleep(10)
-  ui.send_message("webcam_streaming_started", message="")
-  time.sleep(30)
-  ui.send_message("webcam_streaming_stopped", message="")
-  time.sleep(10)
-  ui.send_message("webcam_streaming_started", message="")
-  time.sleep(30)
-  ui.send_message("webcam_streaming_stopped", message="")
-  time.sleep(10)
-  ui.send_message("webcam_disconnected", message="")
-
-App.run(user_loop=user_loop)
+App.run()
