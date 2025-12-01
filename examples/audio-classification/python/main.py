bbad335c0a8f44a3c303141063002959b4669883
Stefano Torneo <s.torneo@arduino.cc>
2025-12-01 09:35:29 +0100

    lines

--- examples/audio-classification/python/main.py
+++ examples/audio-classification/python/main.py
@@ -47,6 +47,7 @@ def on_run_classification(sid, data):
                 response_data['classification'] = { 'class_name': results["class_name"], 'confidence': results["confidence"] }
             else:
                 response_data['error'] = "No objects detected in the audio. Try to lower the confidence threshold."
+
             ui.send_message('classification_complete', response_data, sid)
         else:
             ui.send_message('classification_error', {'message': "No audio available for classification"}, sid)
