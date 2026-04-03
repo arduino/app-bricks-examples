6de2d66eb664b7aa0913859c3f971fc5c5425620
Roberto Gazia <r.gazia@arduino.cc>
2026-04-03 17:03:35 +0200

    Add backend implementation

--- examples/gesture-booth/python/main.py
+++ examples/gesture-booth/python/main.py
@@ -2,11 +2,26 @@
 #
 # SPDX-License-Identifier: MPL-2.0
 
-from arduino.app_utils import App
 from arduino.app_bricks.web_ui import WebUI
-from arduino.app_bricks.video_objectdetection import VideoObjectDetection
+from arduino.app_bricks.hand_gesture_tracking import HandGestureTracking
+from arduino.app_peripherals.camera import Camera
+from arduino.app_utils.image.adjustments import cropped_to_aspect_ratio
+from arduino.app_utils.app import App
+
 
 ui = WebUI()
-detection_stream = VideoObjectDetection(confidence=0.5, debounce_sec=0.0)
+
+camera = Camera(resolution=(1280, 960), fps=30, codec="MJPG", adjustments=cropped_to_aspect_ratio((4, 3)))
+camera.start()
+
+pd = HandGestureTracking(camera)
+pd.on_gesture("None", lambda meta: ui.send_message('gesture_detected', {'gesture': 'None'}))
+pd.on_gesture("Closed_Fist", lambda meta: ui.send_message('gesture_detected', {'gesture': 'Closed Fist'}))
+pd.on_gesture("Open_Palm", lambda meta: ui.send_message('gesture_detected', {'gesture': 'Open Palm'}))
+pd.on_gesture("Pointing_Up", lambda meta: ui.send_message('gesture_detected', {'gesture': 'Pointing Up'}))
+pd.on_gesture("Thumb_Down", lambda meta: ui.send_message('gesture_detected', {'gesture': 'Thumb Down'}))
+pd.on_gesture("Thumb_Up", lambda meta: ui.send_message('gesture_detected', {'gesture': 'Thumb Up'}))
+pd.on_gesture("Victory", lambda meta: ui.send_message('gesture_detected', {'gesture': 'Victory'}))
+pd.on_gesture("ILoveYou", lambda meta: ui.send_message('gesture_detected', {'gesture': 'Rock'}))
 
 App.run()
