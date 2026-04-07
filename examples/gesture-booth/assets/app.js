954e1a0af7c652c155ee67210a6c0fddc20051eb
Damiano Cancemi <damianoc90@gmail.com>
2026-04-07 17:19:19 +0200

    feat: remove arduino.js

--- examples/gesture-booth/assets/app.js
+++ examples/gesture-booth/assets/app.js
@@ -2,24 +2,20 @@
 //
 // SPDX-License-Identifier: MPL-2.0
 
-// Initialize UI
-const ui = new WebUI();
-ui.on_connect(onUIConnected);
-ui.on_disconnect(onUiDisconnected);
+// Initialize WebSocket connection
+const socket = io(`http://${window.location.host}`);
 
-// Listen for analysis results from the backend and display them when received
-ui.on_message('gesture_detected', handleGestureDetected);
-
-// Called when the websocket connection is established
-function onUIConnected() {
+socket.on('connect', () => {
   loadWebcam();
   resetToInitialState();
-}
+});
 
-// Called when the websocket connection is lost
-function onUiDisconnected() {
+socket.on('disconnect', () => {
   content.setAttribute('data-state', 'error');
   title.className = 'title-secondary';
   title.textContent =
     'Connection to the board lost. Please check the connection.';
-}
+});
+
+// Listen for analysis results from the backend and display them when received
+socket.on('gesture_detected', handleGestureDetected);
