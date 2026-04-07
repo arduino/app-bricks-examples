eae89cb47e15d8e4520060e89364f44d7555dec5
Dario Sammaruga <dariosammy@gmail.com>
2026-04-07 15:13:56 +0200

    fix: update import for gesture recognition module

--- examples/gesture-booth/python/main.py
+++ examples/gesture-booth/python/main.py
@@ -3,7 +3,7 @@
 # SPDX-License-Identifier: MPL-2.0
 
 from arduino.app_bricks.web_ui import WebUI
-from arduino.app_bricks.hand_gesture_tracking import HandGestureTracking
+from arduino.app_bricks.gesture_recognition import GestureRecognition
 from arduino.app_peripherals.camera import Camera
 from arduino.app_utils.image.adjustments import cropped_to_aspect_ratio
 from arduino.app_utils.app import App
@@ -14,7 +14,7 @@ ui = WebUI()
 camera = Camera(resolution=(1280, 960), fps=30, codec="MJPG", adjustments=cropped_to_aspect_ratio((4, 3)))
 camera.start()
 
-pd = HandGestureTracking(camera)
+pd = GestureRecognition(camera)
 pd.on_gesture("None", lambda meta: ui.send_message('gesture_detected', {'gesture': 'None'}))
 pd.on_gesture("Closed_Fist", lambda meta: ui.send_message('gesture_detected', {'gesture': 'Closed Fist'}))
 pd.on_gesture("Open_Palm", lambda meta: ui.send_message('gesture_detected', {'gesture': 'Open Palm'}))
