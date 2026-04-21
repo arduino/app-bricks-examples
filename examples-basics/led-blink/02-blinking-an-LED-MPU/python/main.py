8cdb688a73c08f1b0e48cada51ac1f8b9f718876
Fabio C <fabiomassimo.centonze@gmail.com>
2026-04-21 17:24:59 +0200

    update comment example 02

--- examples-basics/led-blink/02-blinking-an-LED-MPU/python/main.py
+++ examples-basics/led-blink/02-blinking-an-LED-MPU/python/main.py
@@ -4,17 +4,15 @@
 
 # Example app to blink the LED 1 mounted on the board.
 import time
-from arduino.app_utils import App
-from arduino.app_utils import Leds
+from arduino.app_utils import App  # Import the App class to create an App Lab application
+from arduino.app_utils import Leds # Import the Leds class to control the LEDs on the board
 
-def loop():
-    # Blink LED 1 in red
-    # Turn on the LED red segment(1, 0, 0)
-    Leds.set_led1_color(1,0,0)
-    time.sleep(1)
-
-    # Turn off the LED (0, 0, 0)
-    Leds.set_led1_color(0,0,0)
+def loop():    
+    # Please note that the Leds.set_led1_color function takes 
+    # three parameters (R, G, B) to set the color of the LED
+    Leds.set_led1_color(1,0,0) # Turn on the LED red segment(1, 0, 0)
+    time.sleep(1)              # Wait for 1 second
+    Leds.set_led1_color(0,0,0) # Turn off the LED (0, 0, 0)
     time.sleep(1)
 
 App.run(user_loop=loop)
