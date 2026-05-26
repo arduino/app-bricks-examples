a35b93e16a334db86255577ce30d94d7ea038563
Dario Sammaruga <dariosammy@gmail.com>
2026-05-26 16:41:27 +0200

    fix: led blink example 04

--- examples-basics/01-led-blink/04-blinking-an-led-with-ui/assets/app.js
+++ examples-basics/01-led-blink/04-blinking-an-led-with-ui/assets/app.js
@@ -2,22 +2,21 @@
 //
 // SPDX-License-Identifier: MPL-2.0
 
-const ledText = document.querySelector('#led-text');
 const ledButton = document.querySelector('#led-button');
+const ledText = document.querySelector('#led-text');
 const errorContainer = document.querySelector('#error-container');
 
 // Initialize UI
 const ui = new WebUI();
 ui.on_connect(onUIConnected);
 ui.on_disconnect(onUIDisconnected);
+
 ui.on_message('led_status_update', updateLedStatus);
 
 // Called when the websocket connection is established.
 function onUIConnected() {
   errorContainer.textContent = '';
   errorContainer.style.display = 'none';
-
-  // Register event listener to LED button
   ledButton.addEventListener('click', handleLedClick);
 
   // Requests the initial state from the board.
@@ -27,7 +26,8 @@ function onUIConnected() {
 // Called when the websocket connection is lost.
 function onUIDisconnected() {
   errorContainer.style.display = 'block';
-  errorContainer.textContent = 'Connection to the board lost. Please check the connection.';
+  errorContainer.textContent =
+    'Connection to the board lost. Please check the connection.';
 }
 
 // Function to update LED status in the UI
