0a345c4d76850b58955ddb90b0df13e0d96bab4b
Dario Sammaruga <dariosammy@gmail.com>
2026-05-27 14:12:01 +0200

    update license headers

--- examples-basics/02-led-matrix/03-led-matrix-animation-mcu/sketch/frames.h
+++ examples-basics/02-led-matrix/03-led-matrix-animation-mcu/sketch/frames.h
@@ -1,6 +1,8 @@
-// SPDX-FileCopyrightText: Copyright (C) Arduino s.r.l. and/or its affiliated companies
-//
-// SPDX-License-Identifier: MPL-2.0
+/*
+ * SPDX-FileCopyrightText: Copyright (C) Arduino s.r.l. and/or its affiliated companies
+ *
+ * SPDX-License-Identifier: MPL-2.0
+ */
 
 // Animation frames for the LED matrix.
 // Each frame is 5 uint32_t values: [pixel0, pixel1, pixel2, pixel3, duration_ms].
--- examples-basics/09-computer-vision/03-image-classification-trigger/sketch/Animation.h
+++ examples-basics/09-computer-vision/03-image-classification-trigger/sketch/Animation.h
@@ -1,1 +1,7 @@
+/*
+ * SPDX-FileCopyrightText: Copyright (C) Arduino s.r.l. and/or its affiliated companies
+ *
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 const uint32_t frame[] = { 0x00000003, 0x601b0202, 0x08203e00, 0x00000000 };
